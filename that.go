4cb99463440a6b2a2d4e3eb7e90d6e064f2347e8
Eric Daniels <eric@erdaniels.com>
2021-02-16 14:15:22 -0500

    use t helper

--- that.go
+++ that.go
@@ -3,6 +3,7 @@ package test
 import "testing"
 
 func That(t *testing.T, actual interface{}, assert func(actual interface{}, expected ...interface{}) string, expected ...interface{}) {
+	t.Helper()
 	if result := assert(actual, expected...); result != "" {
 		t.Fatal(result)
 	}
--- that_test.go
+++ that_test.go
@@ -3,5 +3,5 @@ package test
 import "testing"
 
 func TestThat(t *testing.T) {
-	That(t, 1, ShouldEqual, 1)
+	That(t, 1, ShouldNotEqual, 1)
 }
